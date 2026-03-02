3c27fa584cff53642d7f7dc2a6b69104907261b7
saipavan6635 <saipavansiddam21@gmail.com>
2026-03-02 23:48:42 +0530

    test(controller): add base auth controller test

---
+++ src/test/java/com/rev/app/controller/AuthControllerTest.java
@@ -0,0 +1,13 @@
+package com.rev.app.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class AuthControllerTest {
+
+    @Test
+    public void classShouldBeLoadable() {
+        assertNotNull(AuthController.class);
+    }
+}
