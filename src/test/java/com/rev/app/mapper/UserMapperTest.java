8a3acd65f6a6460987ffbf2bfafee169d4002cf4
saipavan6635 <saipavansiddam21@gmail.com>
2026-03-02 23:50:32 +0530

    test(mapper): add base user mapper test

---
+++ src/test/java/com/rev/app/mapper/UserMapperTest.java
@@ -0,0 +1,13 @@
+package com.rev.app.mapper;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+
+public class UserMapperTest {
+
+    @Test
+    public void classShouldBeLoadable() {
+        assertNotNull(UserMapper.class);
+    }
+}
